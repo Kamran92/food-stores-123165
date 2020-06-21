52954200e34998856ebc3d39d783116e29dfcd48
Kamran92 <k.r.a.92mail.ru>
2020-06-21 12:07:59 +0300

    Updates

---
+++ precache-manifest.f39bdf1f119c984db2865361e6077128.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f6ec1d77a7e8a392d580f4871651ad",
+    "url": "/food-stores/index.html"
+  },
+  {
+    "revision": "7ea34979fbc53da45452",
+    "url": "/food-stores/static/css/main.d767fdb7.chunk.css"
+  },
+  {
+    "revision": "49f069d981848cb8316f",
+    "url": "/food-stores/static/js/2.572d5f32.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/food-stores/static/js/2.572d5f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea34979fbc53da45452",
+    "url": "/food-stores/static/js/main.696c7428.chunk.js"
+  },
+  {
+    "revision": "607d2dbb9b1b30032d90",
+    "url": "/food-stores/static/js/runtime-main.68b89a14.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/food-stores/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/food-stores/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
